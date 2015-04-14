5f1a7bf01d28ec5ce438ee792ff7e1cbbcefaf73
Stuart <stuartbalcombe@gmail.com>
2015-04-14 09:53:06 -0400

    NavBar Initial
    Basic framework for navbars and modals, #preNav complete on @media with
    max-width: 600px needs completing for full responsiveness

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+
+// Open Modal with button //
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
+
+
+
+ $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
