6b0a3a69d2e8eb4991aa8a6aaa4733ec9d7e488f
Shibite <moutahirhicham@gmail.com>
2015-05-19 21:41:53 -0400

    Success page

--- application/views/onboarding/success.php
+++ application/views/onboarding/success.php
@@ -12,71 +12,41 @@
     <meta name="keywords" content="science,chemistry,conference,seminars">
     <meta name="author" content="Stuart Balcombe, Hicham Moutahir, Salim Falah">
     <meta name="robots" content="noindex">    
-      
-    <!-- Global CSS -->
+
     <link rel="stylesheet" href="<?= base_url('css/bootstrap.min.css') ?>">
-    <!-- Theme CSS -->  
     <link rel="stylesheet" href="<?= base_url('css/onboarding.css') ?>">
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//http://www.google-analytics.com/analytics.js','ga');
-
-    ga('create', 'UA-57985737-1', 'auto');
-    ga('send', 'pageview');
-    </script>
-
 </head> 
-
 <body>
-                        <!-- NavBar -->
-        <nav class="navbar navbar-default navbar-fixed-top">
-            <div class="container">
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="#">
-                        <img alt="Brand" src="<?= base_url('images/logo.png') ?>">
-                    </a>
-                </div>
-                <button type="button" class="btn btn-default navbar-btn navbar-right" data-toggle="modal" data-target="#login">Log In</button>
-            </div>
-        </nav>                
+    <div class="card">
+
+        <h1>Success!</h1>
+
+    </div>
 
 
-                        <!-- Content -->
-       <div class="jumbotron">
-            <h1>Value Statement</h1>
-            <p>Social Proof</p>
-            
-            <ul>
-                <li><a href=""><i class="fa fa-facebook"></i></a></li>
-                <li><a href=""><i class="fa fa-twitter"></i></a></li>
-                <li><a href=""><i class="fa fa-linkedin"></i></a></li>
-            </ul>
-        </div>
-
-                        <!-- FOOTER NAVIGATION -->
-
-        <nav class="navbar navbar-default navbar-fixed-bottom" id="footer">
-            <div class="container">
-                <ul>
-                    <li><a href="about.php">About</a></li>
-                    <li><a href="splash.php">Home</a></li>
-                    <li class="end"><a href="upcoming.php">Upcoming</a></li>
-                </ul>
-            </div>
-        </nav>
+    <div class="container text-center explanation">
+        <h3>Congratulations</h3>
+        <h4 class="white-secondary">You have completed your registration process!</h4>
+        <h4 class="white-secondary"><meta http-equiv="refresh" content="5;url=http://www.scholarsintown.com/" />You will be redirected in 5 seconds...</h4>
+    </div>
 
     <!-- Load jQuery before Bootstrap js -->
     <script type="text/javascript" href="<?= base_url('js/jquery.min.js') ?>"></script>
-
     <script type="text/javascript" href="<?= base_url('js/bootstrap.min.js') ?>"></script>
-
     <script type="text/javascript" href="<?= base_url('js/onboarding.js') ?>"></script>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//http://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-57985737-1', 'auto');
+        ga('send', 'pageview');
+    </script>
 </body>
-</html>        
+</html>
